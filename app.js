835f231efe23dc51766a2f70d41874ead396b551
Donna Chen <donnaxtc@gmail.com>
2026-05-22 17:04:01 -0400

    Refactor employee routes to use send method
    Updated response methods to use send instead of json for employee data.

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 import express from "express";
-import employees from "#db/employees";
-
 const app = express();
+export default app;
+
+import employees from "#db/employees";
 
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
 app.get("/employees", (req, res) => {
-  res.json(employees);
+  res.send(employees);
 });
 
-// /random MUST come before /:id so it isn't captured as an id lookup
 app.get("/employees/random", (req, res) => {
   const randomIndex = Math.floor(Math.random() * employees.length);
-  res.json(employees[randomIndex]);
+  res.send(employees[randomIndex]);
 });
 
 app.get("/employees/:id", (req, res) => {
-  const employee = employees.find((e) => e.id === Number(req.params.id));
-  if (!employee) {
-    return res.status(404).send("There is no employee with that id.");
-  }
-  res.json(employee);
-});
+  const { id } = req.params;
 
-export default app;
+  const employee = employees.find((employee) => employee.id === +id);
+
+  if (!employee) {
+    return res.status(404).send("Employee not found");
+  }
+
+  res.send(employee);
+});
